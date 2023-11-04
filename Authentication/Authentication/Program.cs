7cb066658e4b1297a917b6b80720b0bc5c40a361
Alireza Alavi <alternative3004@gmail.com>
2023-11-04 16:09:06 +0330

    Fixed some issues

--- Authentication/Authentication/Program.cs
+++ Authentication/Authentication/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.UseMiddleware<AuthenticationMiddleware>();
-app.UseMiddleware<UserIdMiddleWare>();
+app.UseMiddleware<UserIdMiddleware>();
 
 app.MapGet("/", () => "Hello World!");
 
---
+++ Authentication/Authentication/UserIdMiddleware.cs
@@ -0,0 +1,26 @@
+using System.Security.Claims;
+
+namespace Authentication;
+
+public class UserIdMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public UserIdMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        var userIdClaim = context.User.FindFirst(claim => claim.Type == "UserID");
+
+        if (userIdClaim != null)
+        {
+            var userId = userIdClaim.Value;
+            context.Items["UserID"] = userId;
+        }
+
+        await _next(context);
+    }
+}
