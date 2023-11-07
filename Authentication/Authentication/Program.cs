70a60f9d66768214a8d649950504c5e242451923
s.alireza.alavi <s.alireza.alavi2000@gmail.com>
2023-11-07 12:44:43 +0330

    Modified tests based on built-in package

--- Authentication/Authentication/Program.cs
+++ Authentication/Authentication/Program.cs
@@ -1,9 +1,26 @@
+using System.Text;
 using Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = "Rayvarz",
+            ValidateAudience = false,
+            ValidateLifetime = true
+        };
+    });
+
 var app = builder.Build();
 
-app.UseMiddleware<AuthenticationMiddleware>();
+// app.UseMiddleware<AuthenticationMiddleware>();
+app.UseAuthentication();
 app.UseMiddleware<UserIdMiddleware>();
 
 app.MapGet("/", () => "Hello World!");
