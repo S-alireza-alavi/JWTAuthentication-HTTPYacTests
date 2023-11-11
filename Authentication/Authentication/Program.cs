716dd777598415f9d5c32078a7d6fc189b635c77
s.alireza.alavi <s.alireza.alavi2000@gmail.com>
2023-11-11 09:02:49 +0330

    Used Microsoft built-in package to work with JWTs

--- Authentication/Authentication/Program.cs
+++ Authentication/Authentication/Program.cs
@@ -1,4 +1,5 @@
-using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+using System.Reflection.Metadata;
 using Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
@@ -13,8 +14,49 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             ValidateIssuer = true,
             ValidIssuer = "Rayvarz",
             ValidateAudience = false,
-            ValidateLifetime = true
+            RequireExpirationTime = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = false,
+            SignatureValidator = (token, parameters) =>
+            {
+                SecurityToken jwtSecurityToken = new JwtSecurityToken(token);
+                return jwtSecurityToken;
+            }
         };
+        
+        // JwtBearerEvents jwtBearerEvents = new JwtBearerEvents
+        // {
+        //     OnAuthenticationFailed = delegate(AuthenticationFailedContext context)
+        //     {
+        //         Exception contextException = context.Exception;
+        //         return Task.CompletedTask;
+        //     }
+        // };
+
+        JwtBearerEvents jwtBearerEvents = new JwtBearerEvents
+        {
+            OnAuthenticationFailed = context =>
+            {
+                if (context.Exception is SecurityTokenInvalidIssuerException)
+                {
+                    context.Response.StatusCode = 403;
+                    context.Response.WriteAsync("This token with issuer 'Microsoft' doesn't belong to this domain");
+                }
+                
+                else if (context.Exception is SecurityTokenExpiredException)
+                {
+                    context.Response.StatusCode = 403;
+                    // TODO: Fix this later
+                    // context.Response.WriteAsync("Token Expired");
+                }
+                
+                // Exception contextException = context.Exception;
+
+                return Task.CompletedTask;
+            }
+        };
+
+        options.Events = jwtBearerEvents;
     });
 
 var app = builder.Build();
@@ -23,6 +65,32 @@ var app = builder.Build();
 app.UseAuthentication();
 app.UseMiddleware<UserIdMiddleware>();
 
+// app.MapGet("/", context =>
+// {
+//     if (context.User.Identity is { IsAuthenticated: true })
+//     {
+//         return context.Response.WriteAsync("Hello World!");
+//     }
+//     
+//     return Task.CompletedTask;
+// });
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+// Save this for further usage
+/*
+if (context.User.FindFirst("iat") is { Value: var iatClaimValue })
+        {
+            if (long.TryParse(iatClaimValue, out long iatTimeStamp))
+            {
+                var iatTime = DateTimeOffset.FromUnixTimeSeconds(iatTimeStamp);
+
+                if (iatTime >= DateTime.Now)
+                {
+                    return context.Response.WriteAsync("Refresh Token");
+                }
+            }
+        }
+*/
