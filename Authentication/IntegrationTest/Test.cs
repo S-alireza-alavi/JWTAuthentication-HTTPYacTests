8451533b9d895dbbec8acf3ca71528a3ec231fe9
Alireza Alavi <67781630+S-alireza-alavi@users.noreply.github.com>
2024-01-01 16:50:26 +0330

    Added public and private key to JWTs

--- Authentication/IntegrationTest/Test.cs
+++ Authentication/IntegrationTest/Test.cs
@@ -30,7 +30,7 @@ public class Test
     {
         var request = new HttpRequestMessage(HttpMethod.Get, "/");
         request.Headers.Authorization = new AuthenticationHeaderValue("Bearer",
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiaXNzIjoiUmF5dmFyeiJ9.M217m1Inmi2o4tdAWNaF2cL8mBTiiN2Ahx8PNmK2XRg");
+            "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiaXNzIjoiUmF5dmFyeiJ9.Dz3haMZfXwHOT4_k4nOecpKqz6aXZCIBCMBXX6q2WiZfrVe0M-OjNWiZaXv7ZZ6UtDpC6phgkYBfXgUu_HUX0x1ND694VNvHt2dF3CFsMLJH6HpVLSdXuapzFKFWYF7HI8sTW0nPSAi7QkEezSxuP7QtUR-JjEVO2iX0RaVixrbBnK8hOWj_Gi-1tBiTd3pt_-6Zc41OlAE0CAW8jCxnU60mAjm-QxdoJfiBcb54kM4h-Ih5EWV8kH_AU1l2wRTBJoN7vOxUqdGkJ5HJUMsTGhwjVrobEnyUYh9htwIAfg6gdDsnB8nopMJO1hmdle1LraApNoxz1PsKIQWVdhxygg");
 
         var response = await _httpClient.SendAsync(request);
         
@@ -42,7 +42,7 @@ public class Test
     {
         var request = new HttpRequestMessage(HttpMethod.Get, "/");
         request.Headers.Authorization = new AuthenticationHeaderValue("Bearer",
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZXhwIjoxNjk2MTE4NDAxfQ.UYXfcgd4xNqZ6G1fRbfk1s-7nqLPPbAcIPOoOpR8U8w");
+            "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZXhwIjoxNjk2MTE4NDAxfQ.TPpQMjTbAnh__IAwJRZmsK5XX53KJBxsojHxFCNLPvEQbCldcT49TSQATpG7ZhtQJ61-AZLic2oOGyTCg1pLPEstxZukSoKs3osYp0x8cLNCwx0uJ8ILY8KTTvCDEjQZbEaeepoQe-beK26LT1TKCc2jwzhPqMo45OqifPPgnsdYgRm0efw4JFBgPHgb38Q6n5Cyx09S-Vv4hbTeGu1gcHc5vHUaEqVV_8LG5MkPhd1_WN88kZgM2ADbYRl_pQljTUwV4ev8mKANLvMxLQZ3G_6nNjhieOJfhl0EEHo1rinXHt3qH3z95pST_k9zdJKFmu8mT3_6PUxm0Pbs7Nc3tQ");
 
         var response = await _httpClient.SendAsync(request);
         
@@ -55,7 +55,7 @@ public class Test
     {
         var request = new HttpRequestMessage(HttpMethod.Get, "/GetCurrentUser");
         request.Headers.Authorization = new AuthenticationHeaderValue("Bearer",
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiaXNzIjoiUmF5dmFyeiIsIlVzZXJJRCI6IjEwMDEifQ.3gOITywfQJrzfoBrCF_IMpY-tbHpH1szUY4QvbB2rfs");
+            "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiaXNzIjoiUmF5dmFyeiIsIlVzZXJJRCI6IjEwMDEifQ.buwnGmi-KrCcohWS1_pLVKsgi9v6fvxe-woYmhKOAD-sMXzNLG8H8k-lXWF2YAn7emydxiP8fGhC7dUK8O8Pg6UoQR5u4XNbx210Y-EIK_DAu_T1Vp6yWSTfo_W_i6pxa2nIIRhSge9S1zjIL3luhtZlI0MBSmBYG3NCnqvC_Dy2KxFjAKzbADK4QbbntK841fpihgD8H-WWGcuuw1I_yrV0_M-AgN6ol_te5D8qg9TJZZ1EH9RbBA-abUSOkV39AzEkBH8hQ-3dNN-fkqB6YMFgXwk6ZAAfOaB9iLWqyWM6i0kl1eCcmks278EOO8UFN4ZI3zn-UeZ8sF1h-d10lw");
 
         var response = await _httpClient.SendAsync(request);
         
