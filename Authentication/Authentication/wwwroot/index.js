e6e58e713599b28b609b756f42f00e148600070d
Alireza Alavi <67781630+S-alireza-alavi@users.noreply.github.com>
2024-03-03 11:19:00 +0330

    Removed redundant code and seperated js file

---
+++ Authentication/Authentication/wwwroot/index.js
@@ -0,0 +1,28 @@
+var SUPABASE_URL = 'https://zdgocunjwgxchxbxhytu.supabase.co'
+var SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpkZ29jdW5qd2d4Y2h4YnhoeXR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgyMzA4ODgsImV4cCI6MjAyMzgwNjg4OH0.nczlRY1bsS4_IJfdezci_uLL4tKag2bDgM0qTxDNcOc'
+
+var supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
+
+function logInSubmitted() {
+    const email = document.getElementById('log-in-email').value;
+    const password = document.getElementById('log-in-password').value;
+
+    supabase.auth
+        .signIn({ email, password })
+        .then((response) => {
+            response.error ? alert(response.error.message) : setToken(response);
+            fetchUserDetails();
+        })
+        .catch((err) => {
+            alert(err.response.text);
+        });
+}
+
+function fetchUserDetails () {
+    document.getElementById('user-info').innerText = JSON.stringify(supabase.auth.user());
+}
+
+function setToken(response) {
+    document.querySelector('#generated-token').value = response.session.access_token
+}
